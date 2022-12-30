b8d69f233f61888cc71af5c782729515931ee206
pavitarsharma144 <pavitarsharma144@gmail.com>
2022-12-30 13:31:20 +0530

    generate secret keys

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -6,7 +6,7 @@ module.exports = {
       const payload = {
         id: userId,
       };
-      const secret = "some super secret";
+      const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
         expiresIn: "1h",
         issuer: "jsonwebtoken",
